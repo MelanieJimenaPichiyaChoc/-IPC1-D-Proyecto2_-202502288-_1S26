22bb08b53ff02adb33cfb6924c8e31008a5f129c
Jimena <3502659090401@ingenieria.usac.edu.gt>
2026-04-11 02:55:39 -0600

    Inicio del proyecto

---
+++ src/main/java/com/mycompany/gamezonepro/GameZonePro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gamezonepro;
+
+/**
+ *
+ * @author pichi
+ */
+public class GameZonePro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
